7501668326390e8c3eab2c8ab394161729a55709
rdipardo <59004801+rdipardo@users.noreply.github.com>
2022-03-23 21:45:26 -0400

    Initial commit

---
+++ git-hooks/pre-commit.cmd
@@ -0,0 +1,6 @@
+@echo off
+REM ------------------------------------------------------------
+REM Any copyright is dedicated to the Public Domain.
+REM For more information, please refer to https://unlicense.org.
+REM ------------------------------------------------------------
+python3 %~dp0pre-commit
